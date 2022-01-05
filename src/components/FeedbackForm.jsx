31415d3274b45c397b60bb7b07d8ca2eb351494c
asmansou <asmae.mansouri92@gmail.com>
2022-01-05 14:52:16 +0100

    added global feedback context + loading spinner in list + mock RestAPI with json-server

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,24 @@
 import Card from './shared/Card';
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
+
   const handleText = (e) => {
     if (text === '') {
       setBtnDisabled(true);
@@ -30,7 +41,11 @@ function FeedbackForm({ handleAdd }) {
         rating,
       };
 
-      handleAdd(newFeedback);
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
 
       setText('');
     } else {
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,17 @@
 import FeedbackItem from './FeedbackItem';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
-function FeedbackList({ feedback, handleDelete }) {
-  if (!feedback || feedback.length === 0) return <p>No Feedback Yet</p>;
-  return (
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+
+  if (!isLoading && (!feedback || feedback.length === 0))
+    return <p>No Feedback Yet</p>;
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item, index) => (
@@ -13,7 +21,7 @@ function FeedbackList({ feedback, handleDelete }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem key={index} item={item} handleDelete={handleDelete} />
+            <FeedbackItem key={index} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
